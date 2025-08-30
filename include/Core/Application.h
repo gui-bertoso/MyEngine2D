48e94e54a720ab84c4941ae58e9b5e55d205bb25
gui.bertoso <154436826+gui-bertoso@users.noreply.github.com>
2025-08-29 23:25:26 -0300

    Basic window creation and render

--- include/Core/Application.h
+++ include/Core/Application.h
@@ -2,11 +2,13 @@
 // Created by gbertoso on 2025-08-29.
 //
 #pragma once
+#include <SDL2/SDL.h>
 
 namespace Engine {
     class Application {
     public:
         Application(); // Constructor??
+        ~Application();
         void run(); // Main loop
         void quit(); // Stop a loop here
     private:
@@ -14,5 +16,8 @@ namespace Engine {
         void processInput(); // Processes input
         void update(float deltaTime); // Updates / game logic
         void render(); // Renderization
+
+        SDL_Window* window;
+        SDL_Renderer* renderer;
     };
 }
--- src/Core/Application.cpp
+++ src/Core/Application.cpp
@@ -8,21 +8,60 @@
 #include <thread>
 
 namespace Engine {
-    Application::Application() : isRunning(true) {}
+    Application::Application() : isRunning(true), window(nullptr), renderer(nullptr) {}
+
+    Application::~Application() {
+        if (renderer) SDL_DestroyRenderer(renderer);
+        if (window) SDL_DestroyWindow(window);
+        SDL_Quit();
+    }
 
     void Application::processInput() {
-        std::cout << "Processing inputs..." << std::endl;
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) { // Equal pygame event logics
+                quit();
+            }
+        }
     }
 
     void Application::update(float deltaTime) {
-        std::cout << "Updating game [deltaTime=" << deltaTime << "]" << std::endl;
+        // Logics here
     }
 
     void Application::render() {
-        std::cout << "Rendering frames..." << std::endl;
+        // Clear a screen with a color
+        SDL_SetRenderDrawColor(renderer, 0, 100, 255, 255);
+        SDL_RenderClear(renderer);
+
+        // Draw some itens here (sprites, forms, etc), before SDL Render Present line
+
+        SDL_RenderPresent(renderer); // Type a pygame.window update/flip
     }
 
     void Application::run() {
+        if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+            std::cerr << "SDL could not initialized [SDL_ERROR:" << SDL_GetError() << "]" << std::endl;
+            return;
+        }
+
+        window = SDL_CreateWindow(
+            "Engine",
+            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+            800, 600,
+            SDL_WINDOW_SHOWN
+            );
+
+        if (!window) {
+            std::cerr << "Window could not be created [SDL_ERROR:" << SDL_GetError() << "]" << std::endl;
+            SDL_Quit();
+            return;
+        }
+
+        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+
         using clock = std::chrono::high_resolution_clock;
         auto lastTime = clock::now();
 
